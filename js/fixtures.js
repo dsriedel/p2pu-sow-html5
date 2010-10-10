4c34ed5eba566ffe609361464dd67b097ccbf9fd
dsriedel <riedel.dennis@gmail.com>
2010-10-10 12:56:27 +0200

    Adding the range input element (slider)

---
+++ js/fixtures.js
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dennis
+ * Date: 10-oct-2010
+ * Time: 12:42:41
+ * To change this template use File | Settings | File Templates.
+ */
+var ratingValues = new Array();
+
+ratingValues[0] = "Very bad";
+ratingValues[2] = "Bad";
+ratingValues[4] = "Neutral/Unknown";
+ratingValues[6] = "Good";
+ratingValues[8] = "Very good";
